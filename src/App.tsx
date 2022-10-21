a3a6763133e5fc023f0784cb26089fbd8c23678e
Ridha Chowdhury <chowdhuryridha@gmail.com>
2022-10-21 10:26:05 -0400

    Removed unused modal function in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,43 +23,6 @@ import {
 } from "@chakra-ui/react"
 import {CreateCategoryModal} from "./CreateCategoryModal"
 
-function ModalExample()   {
-  const{isOpen, onOpen, onClose} = useDisclosure()
-
-  return (
-    <Box p={4}>
-      <Button onClick={onOpen}>Open Modal</Button>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Create a new goal</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Stack>
-              <Text>What do you want to call this?</Text>
-              <Input placeholder="Category name"/>
-              <Text>Target minutes per week</Text>
-              <NumberInput placeholder="" step={1} defaultValue={60} min={5}>
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper/>
-                  <NumberDecrementStepper/>
-                </NumberInputStepper>
-              </NumberInput>
-            </Stack>
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-
-    </Box>
-  )
-}
 
 export const App = () => (
   <ChakraProvider theme={theme}>
